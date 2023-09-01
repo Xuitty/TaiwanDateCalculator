8bb614ff3387e68801a1613c408aa550db74edc4
Xuitty <ufokun@gmail.com>
2023-09-02 05:15:29 +0800

    implement minus code, add inputOption and outputOption

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-### 手刻版民國日期計算機
+### DateCalculator(Support Taiwan Year Format)
+
+```
+funtion calculateDate(date, param, inputMode, outputMode)
+```
+
+date: length must greater than 4(ymmdd), y can expand, Ex:19981018
+
+param: param for caculation，Ex:+3,-1,+15,-76....
+
+inputMode: input year format 0=Taiwan format(民國),1=AD
+
+outputMode: convert option 0=original format,1=convert from Taiwan format to AD or AD to Taiwan format
+
+All of the input type are the String type, output also is a String too.
+
+### 手刻版民國西元日期計算機
+
+```
+funtion calculateDate(date, param, inputMode, outputMode)
+```
+
+date:5 位數以上字串，格式為 ymmdd，y 最少一位數
+
+param:加減參數，Ex:+3,-1,+15,-76....
+
+inputMode:0=民國,1=西元
+
+outputMode:0=不轉換,1=民國西元互轉
+
+以上輸入皆為 String，回傳也是 String
---
+++ taiwanDateCalculator.js
@@ -0,0 +1,119 @@
+/**
+ *
+ * @param {String} date 日期，格式ymmdd，y長度可變，至少5位數
+ * @param {String} param 變量，Ex:+10, -23, +5
+ * @param {String} inputMode 輸入模式，0:民國 1:西元
+ * @param {String} outputMode 輸出模式，0:不轉換 1:民國西元互相轉換
+ * @returns {String}
+ */
+
+function calculateDate(date, param, inputMode, outputMode) {
+    console.log(date, param, inputMode, outputMode);
+    let mode = String(param.substring(0, 1));
+    let varibable = Number(param.substring(1));
+    let calYearRaw = Number(date.substring(0, Number(date.length) - 4));
+    let calYear = inputMode === "0" ? calYearRaw + 1911 : calYearRaw;
+    let calMonth = Number(
+        date.substring(Number(date.length) - 4, Number(date.length) - 2)
+    );
+    let calDay = Number(
+        date.substring(Number(date.length) - 2, Number(date.length))
+    );
+    let normalDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let leapDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    let isLeapYear = isLeapYearFunc(calYear);
+    let currentYearDays = isLeapYear ? leapDays : normalDays;
+    let dateResult = "ERR";
+
+    if (
+        calDay < 1 ||
+        calDay > currentYearDays[calMonth - 1] ||
+        calMonth < 1 ||
+        calMonth > 12 ||
+        date.length < 5 ||
+        (mode != "+" && mode != "-")
+    ) {
+        return dateResult;
+    }
+    if (mode === "+") {
+        calDay = calDay + varibable;
+        while (calDay > currentYearDays[calMonth - 1]) {
+            calDay = calDay - currentYearDays[calMonth - 1];
+            calMonth++;
+            if (calMonth > 12) {
+                calMonth = 1;
+                calYear++;
+                isLeapYear = isLeapYearFunc(calYear);
+                currentYearDays = isLeapYear ? leapDays : normalDays;
+            }
+        }
+
+        if (
+            (inputMode === "0" && outputMode === "0") ||
+            (inputMode === "1" && outputMode === "1")
+        ) {
+            dateResult =
+                String(calYear - 1911) +
+                String(calMonth).padStart(2, "0") +
+                String(calDay).padStart(2, "0");
+            return dateResult;
+        }
+
+        dateResult =
+            String(calYear) +
+            String(calMonth).padStart(2, "0") +
+            String(calDay).padStart(2, "0");
+        return dateResult;
+    }
+    calDay = calDay - varibable;
+    while (calDay < 1) {
+        calDay =
+            calDay +
+            (calMonth - 2 < 0
+                ? currentYearDays[11]
+                : currentYearDays[calMonth - 1 - 1]);
+        calMonth--;
+        if (calMonth < 1) {
+            calMonth = 12;
+            calYear--;
+            isLeapYear = isLeapYearFunc(calYear);
+            currentYearDays = isLeapYear ? leapDays : normalDays;
+        }
+    }
+    if (outputMode === "0") {
+        dateResult =
+            String(calYear) +
+            String(calMonth).padStart(2, "0") +
+            String(calDay).padStart(2, "0");
+        return dateResult;
+    }
+
+    if (inputMode === "0") {
+        dateResult =
+            String(calYear + 1911) +
+            String(calMonth).padStart(2, "0") +
+            String(calDay).padStart(2, "0");
+        return dateResult;
+    }
+
+    if (inputMode === "1") {
+        dateResult =
+            String(calYear - 1911) +
+            String(calMonth).padStart(2, "0") +
+            String(calDay).padStart(2, "0");
+        return dateResult;
+    }
+}
+
+function isLeapYearFunc(year) {
+    let result =
+        year % 4 === 0
+            ? year % 100 === 0
+                ? year % 400 === 0
+                    ? true
+                    : false
+                : true
+            : false;
+    return result;
+}
